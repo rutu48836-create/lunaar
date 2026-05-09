4ee538e935e5f7646215393dac5801962212a09b
rutu48836-create <rutu48836@gmail.com>
2026-05-09 10:21:32 +0530

    Update Nav.jsx

--- src/Frontend/Components/Nav.jsx
+++ src/Frontend/Components/Nav.jsx
@@ -2,9 +2,11 @@
 import { useState } from "react"
 import { SendHorizonal, ChevronDown, Menu, X } from "lucide-react"
 import styles from "../Styles/Navbar.module.css"
+import {useNavigate} from "react-router-dom"
 
 export function LandingNavbar() {
   const [mobileOpen, setMobileOpen] = useState(false)
+  const navigate = useNavigate()
 
   const navLinks = [
   { label: "About Us", href: "/about" },
@@ -21,15 +23,15 @@ export function LandingNavbar() {
         </a>
 
         <div className={styles.links}>
-          <button className={styles.link}>About Us</button>
-          <button className={styles.link}>Contact Us</button>
-          <button className={styles.link}>Policy</button>
+          <button className={styles.link} href="/About">About Us</button>
+          <button className={styles.link} href="/Contact">Contact Us</button>
+          <button className={styles.link} href="/policy">Policy</button>
    
         </div>
 
         <div className={styles.right}>
-          <button className={styles.ghost}>Sign in</button>
-          <button className={styles.primary}>Get started →</button>
+          <button className={styles.ghost} onClick={() => navigate('/Dashboard')}>Sign in</button>
+          <button className={styles.primary} onClick={() => navigate('/Dashboard')}>Get started →</button>
         </div>
 
         <button className={styles.hamburger} onClick={() => setMobileOpen(p => !p)}>
